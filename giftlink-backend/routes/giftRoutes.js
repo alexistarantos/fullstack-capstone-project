1f2d815ac0851d7704c3dda6bf5481f4b6bd3ecd
Alexis Tarantos <77683837+alexistarantos@users.noreply.github.com>
2026-07-22 00:06:03 +0300

    Refactor giftRoutes.js for database connection

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+
 router.get('/', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
@@ -40,8 +44,6 @@ router.get('/:id', async (req, res) => {
         res.status(500).send('Error fetching gift');
     }
 });
-
-
 
 // Add a new gift
 router.post('/', async (req, res, next) => {
